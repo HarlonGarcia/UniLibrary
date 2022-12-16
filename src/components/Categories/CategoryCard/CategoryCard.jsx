e960784c74e5d35288ae7975bbfa81348a677016
Harlon Garcia <harlondesigner@gmail.com>
2022-12-16 17:38:49 -0300

    feat: make home page responsive

--- src/components/Categories/CategoryCard/CategoryCard.jsx
+++ src/components/Categories/CategoryCard/CategoryCard.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import styles from './CategoryCard.module.scss'
+import React from "react";
+import styles from "./CategoryCard.module.scss";
 
-const CategoryCard = ({icon, label}) => {
-  
+const CategoryCard = ({ icon, label }) => {
   const handleClick = () => {
-    console.log('CategoryCard');
-  }
+    console.log("CategoryCard");
+  };
 
   return (
     <div className={styles.category} onClick={handleClick}>
-        <div className={styles.flip__card}>
-            <div className={styles.flip__card__front}>
-                {icon}
-            </div>
-            <div className={styles.flip__card__back}>
-                <h4>{label}</h4>
-            </div>
+      <div className={styles.flip__card}>
+        <div className={styles.flip__card__front}>{icon}</div>
+        <div className={styles.flip__card__back}>
+          <h4>{label}</h4>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CategoryCard;
